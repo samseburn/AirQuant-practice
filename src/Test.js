030a3b611485e6c7136e4764b71e29e3da32b5a7
eunye <dms8945@gmail.com>
2023-01-02 10:50:51 +0900

    📝 Test: 수정

--- src/Test.js
+++ src/Test.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Test = () => {
-	return <div>Test 파일 입니다 수정했습니다</div>;
+	return (
+		<div>
+			<h1>안녕하세요</h1>
+			<p>Test 파일 입니다</p>
+			<p>-작성자: 박은예-</p>
+		</div>
+	);
 };
 
 export default Test;
