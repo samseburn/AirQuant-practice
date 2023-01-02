deb23de51198aed65cb645950198209369a752b1
seoyoon <tkddnjs1406@icloud.com>
2023-01-02 10:48:10 +0900

    ✅ test: hi

--- src/Seoyoon/SeoyoonTest.jsx
+++ src/Seoyoon/SeoyoonTest.jsx
@@ -4,6 +4,14 @@ const SeoyoonTest = () => {
     return (
         <div>
             제발 잘 좀 해줘..~~
+            <table>
+                <tr>
+                    <td>안녕</td>
+                    <td>하세</td>
+                    <td>용</td>
+                </tr>
+            </table>
+            <h3>응애 응애 🔥</h3>
         </div>
     );
 };
