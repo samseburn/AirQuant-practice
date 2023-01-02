e5299f650a399c22ae6b905c7f9c2d6d8b989dce
hwayoung <hyriver37@gamil.com>
2023-01-02 11:37:39 +0900

    test: test

--- src/test/Hwayoung.jsx
+++ src/test/Hwayoung.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Hwayoung = () => {
-	return <div>화영 테스트 스타벅스</div>;
+	return <div>화영 테스트 스타벅스 다이어리</div>;
 };
 
 export default Hwayoung;
--- src/test/Seokyoung.jsx
+++ src/test/Seokyoung.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Seokyoung = () => {
-	return <div>화영과 석영의 깃 테스트를 위한위핞위</div>;
+	return <div>화영과 석영의 깃 테스트를 배고파 배고파 커피</div>;
 };
 
 export default Seokyoung;
